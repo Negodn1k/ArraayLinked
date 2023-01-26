ed134364c98ab464b0da7450a8ba00d9b59a871b
Negodn1k <havkaset@gmail.com>
2023-01-26 15:16:46 +0300

    v 1.0

--- src/Randomize.java
+++ src/Randomize.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Randomize {
+public class Randomize {
+    public int random() {
+        return (int) (Math.random() * 10);
+    }
+    public int randomHunT() {
+        return (int) (Math.random() * 100000);
+    }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Task {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Task extends Randomize{
+    int i = 0;
+    public void array(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (i <= 100000) {
+            arr.add(random());
+            i++;
+        }
+
+        i = 0;
+        while (i <= 10000) {
+            System.out.print(arr.get(randomHunT()));
+            i++;
+        }
+    }
+    public void linked() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        while (i <= 100000) {
+            linkedList.add(random());
+            i++;
+        }
+
+        i = 0;
+        while (i <= 10000) {
+            System.out.print(linkedList.get(randomHunT()));
+            i++;
+        }
+    }
 }
